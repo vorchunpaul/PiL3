281cb4332417fbd3e04d78d676369adfdabde1a8
paul vorchun <vorchun@tuta.io>
2022-05-21 18:59:02 +0300

    1.2

---
+++ part1/1.2.lua
@@ -0,0 +1,24 @@
+function twice (x)
+    return 2 * x
+end
+
+--[[
+
+run script 
+
+1)
+
+    $ lua
+    > dofile('part1//1.2.lua') 
+    > twice(2)
+    4
+
+2)
+
+    $ lua -i .\part1\1.2.lua
+    > twice(2)
+    4
+
+faster is 2 btw no`t print function to call
+
+--]]
