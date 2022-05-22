eb257c2658465c1687394a512332b1f951304bbb
paul vorchun <vorchun@tuta.io>
2022-05-22 22:16:02 +0300

    4.3

---
+++ part4/4.3.lua
@@ -0,0 +1,10 @@
+--[[
+
+    # repeat until problem 
+
+    im think, repeat must use in repl or like pepl task.
+    repeat until not may replase with other stagmention
+    without goto, but goto is bad, so repeat must be in 
+    lua
+    
+]]
